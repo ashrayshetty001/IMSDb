2f3f72cc5b0a5ba3f9711722268dc8c8b231f0c9
Ashray Shetty <22a21.ashray@sjec.ac.in>
2026-03-11 12:02:38 +0530

    update db

---
+++ IMSDb.WebApp/Migrations/20260311062736_AddUserAccountsTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IMSDb.WebApp.Migrations
+{
+    
+    public partial class AddUserAccountsTable : Migration
+    {
+        
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "user_accounts",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Role = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_accounts", x => x.id);
+                });
+        }
+
+        
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_accounts");
+        }
+    }
+}
