21f5e5fe5820c1617842f57d5ef730994b9ef773
Ashray Shetty <22a21.ashray@sjec.ac.in>
2026-03-11 11:35:07 +0530

    update

--- IMSDb.WebApp/ViewModel/LoginViewModel.cs
+++ IMSDb.WebApp/ViewModel/LoginViewModel.cs
@@ -5,9 +5,9 @@ namespace IMSDb.WebApp.ViewModels
     public class LoginViewModel
     {
         [Required(ErrorMessage = "Username is required")]
-        public string UserName { get; set; }
+        public string? UserName { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
-        public string Password { get; set; }
+        public string? Password { get; set; }
     }
 }
