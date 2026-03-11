d44ad7b643896dac09b004f9945f1cb3a51aebbf
Ashray Shetty <22a21.ashray@sjec.ac.in>
2026-03-11 15:38:55 +0530

    updates

--- IMSDb.WebApp/Components/Models/Product.cs
+++ IMSDb.WebApp/Components/Models/Product.cs
@@ -5,6 +5,6 @@
         public Guid Id { get; set; }
         public string Name { get; set; }
         public int Quantity { get; set; }
-        public double Price { get; set; }
+        public float Price { get; set; }
     }
 }
