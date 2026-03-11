0359fac02756cf008f488d20e91a7c18430542dc
Ashray Shetty <22a21.ashray@sjec.ac.in>
2026-03-11 15:51:21 +0530

    corrected code

---
+++ IMSDb.WebApp/Migrations/20260311101418_UpdatePriceToDouble.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IMSDb.WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePriceToDouble : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Price",
+                table: "Product",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Product",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+    }
+}
